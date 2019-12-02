5727fc7a22ec0928032612006fcdfe57091f7bf4
Houlgatte Louka <bazar.tri@gmail.com>
2019-12-02 22:14:27 +0100

    VideosController ImagesController -> beginning of delete and get

--- src/routes/Images.ts
+++ src/routes/Images.ts
@@ -1,9 +1,7 @@
 import { logger } from '@shared';
 import { Request, Response, Router } from 'express';
 import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
-import { paramMissingError } from '@shared';
 import { ParamsDictionary } from 'express-serve-static-core';
-import S3 from 'aws-sdk/clients/s3';
 import {uuid} from 'uuidv4';
 import multer from 'multer';
 import pkgcloud from 'pkgcloud';
@@ -18,57 +16,52 @@ const router = Router();
 
 const FOLDER = 'images';
 const BUCKET = process.env.SCALEWAY_BUCKET;
+const CONTAINER = (BUCKET || '').concat('/').concat(FOLDER);
 
-const getS3Bucket = (): any => {
-    return new S3(
-      {
-          accessKeyId: process.env.SCALEWAY_ACCESS_KEY_ID,
-          secretAccessKey: process.env.SCALEWAY_SECRET_ACCESS_TOKEN,
-      });
-};
-/******************************************************************************
- *                      Get All Users - "GET /api/users/all"
- ******************************************************************************/
-
-router.get('/all', async (req: Request, res: Response) => {
-
-    // const getPromise = new AWS.S3({
-    //     apiVersion: '2006-03-01',
-    //     endpoint: AWS_ENDPOINT,
-    // })
-    //   .getObject()
-    //   .promise();
-    // getPromise.then((data) => {
-    //     logger.info(data);
-    // });
-
-    // try {
-    //     const users = await userDao.getAll();
-    //     return res.status(OK).json({users});
-    // } catch (err) {
-    //     logger.error(err.message, err);
-    //     return res.status(BAD_REQUEST).json({
-    //         error: err.message,
-    //     });
-    // }
+const CLIENT = pkgcloud.storage.createClient({
+    provider: 'amazon',
+    keyId: process.env.SCALEWAY_ACCESS_KEY_ID,
+    key: process.env.SCALEWAY_SECRET_ACCESS_TOKEN,
+    // @ts-ignore
+    endpoint: process.env.SCALEWAY_ENDPOINT,
 });
 
 /******************************************************************************
- *                       Add One - "POST /api/users/add"
+ *                      Get One Picture - "GET /images/{:id}"
+ ******************************************************************************/
+
+router.get('/:fileId', async (req: Request, res: Response) => {
+        const {fileId} = req.params as ParamsDictionary;
+        const readStream = CLIENT.download({
+            container: CONTAINER,
+            remote: fileId,
+        });
+        readStream.on('error', (err) => {
+            return res.status(BAD_REQUEST).json({
+                error: err.message,
+            });
+        });
+        readStream.on('success', (file) => {
+            // tslint:disable-next-line:no-console
+            console.log(file);
+            return res.status(OK).json({
+                image: file,
+            });
+            // success, file will be a File model
+        });
+});
+
+/******************************************************************************
+ *                       Add One Image - "POST /images"
  ******************************************************************************/
 
 router.post('', upload.single('imagePosted'), async (req: Request, res: Response) => {
-    const filename = uuid();
-    const client = pkgcloud.storage.createClient({
-        provider: 'amazon',
-        keyId: process.env.SCALEWAY_ACCESS_KEY_ID,
-        key: process.env.SCALEWAY_SECRET_ACCESS_TOKEN,
-        // @ts-ignore
-        endpoint: process.env.SCALEWAY_ENDPOINT,
-    });
+    const nameArray = req.file.originalname.split('.');
+    const extension = '.'.concat(nameArray[nameArray.length - 1]);
     const readStream = require('streamifier').createReadStream(req.file.buffer);
-    const writeStream = client.upload({
-        container: (BUCKET || '').concat('/').concat(FOLDER),
+    const filename = uuid().concat(extension);
+    const writeStream = CLIENT.upload({
+        container: CONTAINER,
         remote: filename,
     });
     writeStream.on('error', (err) => {
@@ -77,52 +70,27 @@ router.post('', upload.single('imagePosted'), async (req: Request, res: Response
         });
     });
 
-    writeStream.on('success', (file) => {
+    writeStream.on('success', () => {
         return res.status(CREATED).json({
             contentId: filename,
         });
         // success, file will be a File model
     });
-
     readStream.pipe(writeStream);
 });
 
 /******************************************************************************
- *                       Update - "PUT /api/users/update"
+ *                    Delete One Image - "DELETE /images/:id"
  ******************************************************************************/
 
-router.put('/update', async (req: Request, res: Response) => {
-    try {
-        const { user } = req.body;
-        if (!user) {
-            return res.status(BAD_REQUEST).json({
-                error: paramMissingError,
-            });
-        }
-        user.id = Number(user.id);
-        return res.status(OK).end();
-    } catch (err) {
-        logger.error(err.message, err);
+router.delete('/:fileId', async (req: Request, res: Response) => {
+    const { fileId } = req.params as ParamsDictionary;
+    CLIENT.removeFile(CONTAINER, fileId, (err) => {
         return res.status(BAD_REQUEST).json({
-            error: err.message,
+            error: err,
         });
-    }
-});
-
-/******************************************************************************
- *                    Delete - "DELETE /api/users/delete/:id"
- ******************************************************************************/
-
-router.delete('/delete/:id', async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params as ParamsDictionary;
-        return res.status(OK).end();
-    } catch (err) {
-        logger.error(err.message, err);
-        return res.status(BAD_REQUEST).json({
-            error: err.message,
-        });
-    }
+    });
+    return res.status(OK).end();
 });
 
 /******************************************************************************
--- src/routes/Videos.ts
+++ src/routes/Videos.ts
@@ -1,27 +1,96 @@
 import { logger } from '@shared';
-import { Request, Response, Router, Express } from 'express';
+import { Request, Response, Router } from 'express';
 import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
-import { paramMissingError } from '@shared';
 import { ParamsDictionary } from 'express-serve-static-core';
-import AWS, {AWSError} from 'aws-sdk';
-import S3, {GetObjectRequest} from 'aws-sdk/clients/s3';
-import {GetObjectResponse} from 'aws-sdk/clients/mediastoredata';
+import {uuid} from 'uuidv4';
+import multer from 'multer';
+import pkgcloud from 'pkgcloud';
+
+const upload = multer({
+    dest: 'upload/',
+    storage: multer.memoryStorage(),
+});
 
 // Init shared
 const router = Router();
 
 const FOLDER = 'videos';
 const BUCKET = process.env.SCALEWAY_BUCKET;
-const AWS_ENDPOINT = new AWS.Endpoint('https://s3.'.concat(process.env.SCALEWAY_REGION || '').concat('.scw.cloud'));
+const CONTAINER = (BUCKET || '').concat('/').concat(FOLDER);
 
-const getS3Bucket = (): any => {
-  return new S3(
-    {
-      accessKeyId: process.env.SCALEWAY_ACCESS_KEY_ID,
-      secretAccessKey: process.env.SCALEWAY_SECRET_ACCESS_TOKEN,
-      region: process.env.SCALEWAY_REGION,
+const CLIENT = pkgcloud.storage.createClient({
+    provider: 'amazon',
+    keyId: process.env.SCALEWAY_ACCESS_KEY_ID,
+    key: process.env.SCALEWAY_SECRET_ACCESS_TOKEN,
+    // @ts-ignore
+    endpoint: process.env.SCALEWAY_ENDPOINT,
+});
+
+/******************************************************************************
+ *                      Get One Video - "GET /videos/{:id}"
+ ******************************************************************************/
+
+router.get('/:fileId', async (req: Request, res: Response) => {
+    const {fileId} = req.params as ParamsDictionary;
+    const readStream = CLIENT.download({
+        container: CONTAINER,
+        remote: fileId,
     });
-};
+    readStream.on('error', (err) => {
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    });
+    readStream.on('success', (file) => {
+        // tslint:disable-next-line:no-console
+        console.log(file);
+        return res.status(OK).json({
+            image: file,
+        });
+        // success, file will be a File model
+    });
+});
+
+/******************************************************************************
+ *                       Add One Video - "POST /videos"
+ ******************************************************************************/
+
+router.post('', upload.single('imagePosted'), async (req: Request, res: Response) => {
+    const nameArray = req.file.originalname.split('.');
+    const extension = '.'.concat(nameArray[nameArray.length - 1]);
+    const readStream = require('streamifier').createReadStream(req.file.buffer);
+    const filename = uuid().concat(extension);
+    const writeStream = CLIENT.upload({
+        container: CONTAINER,
+        remote: filename,
+    });
+    writeStream.on('error', (err) => {
+        return res.status(BAD_REQUEST).json({
+            error: err.message,
+        });
+    });
+
+    writeStream.on('success', () => {
+        return res.status(CREATED).json({
+            contentId: filename,
+        });
+        // success, file will be a File model
+    });
+    readStream.pipe(writeStream);
+});
+
+/******************************************************************************
+ *                    Delete One Video - "DELETE /videos/:id"
+ ******************************************************************************/
+
+router.delete('/:fileId', async (req: Request, res: Response) => {
+    const { fileId } = req.params as ParamsDictionary;
+    CLIENT.removeFile(CONTAINER, fileId, (err) => {
+        return res.status(BAD_REQUEST).end();
+    });
+    return res.status(OK).end();
+});
+
 /******************************************************************************
  *                                     Export
  ******************************************************************************/
