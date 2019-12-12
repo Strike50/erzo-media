16bf45afd2db1663d649adca8a45e0613f955e5a
Laplo <ronanlaplaud4@gmail.com>
2019-12-12 21:05:20 +0100

    add keycloak, 1 fake test, dockerfile and update gitlab-ci.yml

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express';
 import ImageRouter from './Images';
 import VideoRouter from './Videos';
+import {KeycloakMiddleware} from '../shared/Keycloak';
 
 // Init router and path
 const router = Router();
 
+const keycloak = KeycloakMiddleware.getInstance();
 // Add sub-routes
-router.use('/images', ImageRouter);
-router.use('/videos', VideoRouter);
+const usingMockDb = (process.env.USE_MOCK_DB || '').toLowerCase();
+if (usingMockDb === 'true') {
+    router.use('/images', ImageRouter);
+    router.use('/videos', VideoRouter);
+} else {
+    router.use('/images', keycloak.protect(), ImageRouter);
+    router.use('/videos', keycloak.protect(), VideoRouter);
+}
 
 // Export the base-router
 export default router;
