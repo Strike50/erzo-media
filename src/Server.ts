476474f7e900f1ae3b34630c739b3dda96229b18
Houlgatte Louka <bazar.tri@gmail.com>
2020-01-15 13:54:40 +0100

    Protect routes WIP Delete img and videos

--- src/Server.ts
+++ src/Server.ts
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors({
         origin: allowedOrigins,
         exposedHeaders: 'Content-Location',
+        credentials: true,
     }));
 } else {
     app.use(cors({
@@ -27,22 +28,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-/**
- * Point express to the 'views' directory. If you're using a
- * single-page-application framework like react or angular
- * which has its own development server, you might want to
- * configure this to only serve the index file while in
- * production mode.
- */
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
 
 // Export express instance
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,14 +8,8 @@ const router = Router();
 
 const keycloak = KeycloakMiddleware.getInstance();
 // Add sub-routes
-const usingMockDb = (process.env.USE_MOCK_DB || '').toLowerCase();
-if (usingMockDb === 'true') {
-    router.use('/images', ImageRouter);
-    router.use('/videos', VideoRouter);
-} else {
-    router.use('/images', keycloak.protect(), ImageRouter);
-    router.use('/videos', keycloak.protect(), VideoRouter);
-}
+router.use('/images', keycloak.protect(), ImageRouter);
+router.use('/videos', keycloak.protect(), VideoRouter);
 
 // Export the base-router
 export default router;
